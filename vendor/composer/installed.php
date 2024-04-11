29070394b21fa4b134633d7b6c7e64df7367ed86
Caio Enrique Neves Araújo <caioenrique.na@gmail.com>
2024-04-11 16:23:41 -0300

    implemento de msg para whatsapp

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '446d1846c1dadaef3d412d76097bcd0bc967edc4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '446d1846c1dadaef3d412d76097bcd0bc967edc4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.0.0',
+            'version' => '8.0.0.0',
+            'reference' => '750ecf56e1a8627823d182ed3b3d6e7f79e41820',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
