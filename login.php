bcfe23180b41c0f7c850942e9e99f8352cc4256f
Andre Moreira <andreluiz.moreira@hotmail.com>
2024-05-27 21:58:55 -0300

    alteracao login

--- login.php
+++ login.php
@@ -18,16 +18,19 @@ if(isset($_SESSION['usuario_id']) && $_SESSION['usuario_id'] != null ){
 </head>
 <body>
     <div class="container">
+        <a href="index.html" class="logo">
+            <img src="img/logo.png" alt="Logo">
+        </a>
         <form action="verifica-login.php" method="POST" class="login-form">
             <fieldset>
                 <legend>Login</legend>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="text" class="form-control" id="email" name="email" required>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="exemplo@gmail.com" required>
                 </div>
                 <div class="mb-3">
                     <label for="senha" class="form-label">Senha</label>
-                    <input type="password" class="form-control" id="senha" name="senha" required>
+                    <input type="password" class="form-control" id="senha" name="senha" placeholder="********" required>
                 </div>
                 <div class="text-center">
                     <a href="cadastro_usuario.html">Ainda não possui login? Cadastre-se</a> <br><br>
